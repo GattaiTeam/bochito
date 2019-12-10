ad006ed4a907454486d4098baf245fc744536cab
Red <rogeliorv@gmail.com>
2019-12-10 19:38:53 +0200

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/red/Documents/change/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/red/Documents/gattai/flutterlogin"
+export "FLUTTER_TARGET=/Users/red/Documents/gattai/flutterlogin/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/red/Documents/change/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
