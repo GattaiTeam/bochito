dc9af262912f9ca1e969781939d036f9fcc22aab
Carlos Isaias Ruiz <zimoo354@MacBook-Pro-de-Carlos-2.local>
2020-01-17 23:49:28 -0600

    Created validator function and called in multiple forms

--- lib/src/helpers/validator.dart
+++ lib/src/helpers/validator.dart
@@ -1,6 +1,11 @@
+Map regex = {
+  'email': RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"),
+};
 
-bool isEmail(String em) {
-  return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(em);
+String validator(String value, String type) {
+  if (value.isEmpty) return "Required field";
+
+  return !regex[type].hasMatch(value) ? "Please enter a valid " + type : null;
 }
 
 
