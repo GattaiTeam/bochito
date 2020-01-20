49793c3404efced4981f217a4e33269dcb25a803
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2020-01-20 11:05:32 -0700

    ADDED Latest commits with new screen for Launch

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,6 +2,7 @@ package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
 import rocks.biessek.fluttercountrypicker.FlutterCountryPickerPlugin;
+import com.sahdeepsingh.liquid_swipe.LiquidSwipePlugin;
 
 /**
  * Generated file. Do not edit.
@@ -12,6 +13,7 @@ public final class GeneratedPluginRegistrant {
       return;
     }
     FlutterCountryPickerPlugin.registerWith(registry.registrarFor("rocks.biessek.fluttercountrypicker.FlutterCountryPickerPlugin"));
+    LiquidSwipePlugin.registerWith(registry.registrarFor("com.sahdeepsingh.liquid_swipe.LiquidSwipePlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
